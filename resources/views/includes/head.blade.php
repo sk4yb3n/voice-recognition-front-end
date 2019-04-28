e703429fd45849aa5839cec22919806965fa6ea2
Emir Kurtanovic <sk4yb3n@hotmail.com>
2019-04-28 14:00:18 +0200

    project init

---
+++ resources/views/includes/head.blade.php
@@ -0,0 +1,8 @@
+<meta charset="utf-8">
+<meta name="description" content="">
+<meta name="author" content="Emir">
+
+<title>Voice Recognition</title>
+
+<!-- Init files -->
+<link rel="stylesheet" href="{{ mix('/css/app.css') }}">
